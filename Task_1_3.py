7a8324b497d1d96494e1550d9d5545e7393003a5
Daniil Frolov <frolofelo@gmail.com>
2023-04-27 11:31:24 +0300

    Писал использую только if elif else... Хочу циклы и min(), max()

--- Task_1_3.py
+++ Task_1_3.py
@@ -6,7 +6,7 @@ a4 = x / y
 a5 = x // y
 result = [a1, a2, a3, a4, a5]
 res1 = a1
-res2 = 0
+res2 = -10000
 
 if a2 >= res1:
     res2 = res1
@@ -38,6 +38,50 @@ elif a3 >= res1:
         if a5 >= res1:
             res2 = res1
             res1 = a5
+    elif a5 >= res1:
+        res2 = res1
+        res1 = a5
+elif a4 >= res1:
+    res2 = res1
+    res1 = a4
+    if a5 >= res1:
+        res2 = res1
+        res1 = a5
+elif a5 >= res1:
+    res2 = res1
+    res1 = a5
+
+else:
+    if a2 >= res2:
+        res2 = a2
+        if a3 >= res2:
+            res2 = a3
+            if a4 >= res2:
+                res2 = a4
+                if a5 >= res2:
+                    res2 = a5
+            elif a5 >= res2:
+                res2 = a5
+        elif a4 >= res2:
+            res2 = a4
+            if a5 >= res2:
+                res2 = a5
+        elif a5 >= res2:
+            res2 = a5
+    elif a3 >= res2:
+        res2 = a3
+        if a4 >= res2:
+            res2 = a4
+            if a5 >= res2:
+                res2 = a5
+        elif a5 >= res2:
+            res2 = a5
+    elif a4 >= res2:
+        res2 = a4
+        if a5 >= res2:
+            res2 = a5
+    elif a5 >= res2:
+        res2 = a5
 
 print(f'Максимум: {res1}, второй максимум: {res2}')
 print(f'Список чисел для удобства проверки: {result}')
