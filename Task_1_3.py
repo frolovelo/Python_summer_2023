cedf277fd1deeb9ae6c4775f403160d55031f6ec
Daniil Frolov <frolofelo@gmail.com>
2023-04-27 21:51:23 +0300

    Кажется понял)

--- Task_1_3.py
+++ Task_1_3.py
@@ -8,25 +8,25 @@ result = [a1, a2, a3, a4, a5]
 res1 = -10000
 res = a1
 if a2 > res:
+    res1 = res
     res = a2
+elif a2 > res1:
+    res1 = a2
 if a3 > res:
+    res1 = res
     res = a3
-if a4 > res:
-    res = a4
-if a5 > res:
-    res = a5
-
-if res1 < a1 < res:
-    res1 = a1
-if res1 < a2 < res:
-    res1 = a2
-if res1 < a2 < res:
-    res1 = a2
-if res1 < a3 < res:
+elif a3 > res1:
     res1 = a3
-if res1 < a4 < res:
+if a4 > res:
+    res1 = res
+    res = a4
+elif a4 > res1:
     res1 = a4
-if res1 < a5 < res:
+if a5 > res:
+    res1 = res
+    res = a5
+elif a5 > res1:
     res1 = a5
+
 print('Второй максимум:', res1)
 print(f'Список чисел для удобства проверки: {result}')
