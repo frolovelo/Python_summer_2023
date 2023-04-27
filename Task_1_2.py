51a5bdf16f01429e63902cb53c05bd92965dca42
Daniil Frolov <frolofelo@gmail.com>
2023-04-27 11:36:03 +0300

    Писал используя только if elif else...

--- Task_1_2.py
+++ Task_1_2.py
@@ -5,44 +5,36 @@ a3 = x * y
 a4 = x / y
 a5 = x // y
 result = [a1, a2, a3, a4, a5]
-res = 0
+res1 = a1
 
-if a1 >= a2:
-    if a1 >= a3:
-        if a1 >= a4:
-            if a1 >= a5:
-                res = a1
-            else:
-                res = a5
+if a2 >= res1:
+    res1 = a2
+    if a3 >= res1:
+        res1 = a3
+        if a4 >= res1:
+            res1 = a4
+            if a5 >= res1:
+                res1 = a5
+        elif a5 >= res1:
+            res1 = a5
+    elif a4 >= res1:
+        res1 = a4
+        if a5 >= res1:
+            res1 = a5
+elif a3 >= res1:
+    res1 = a3
+    if a4 >= res1:
+        res1 = a4
+        if a5 >= res1:
+            res1 = a5
+    elif a5 >= res1:
+        res1 = a5
+elif a4 >= res1:
+    res1 = a4
+    if a5 >= res1:
+        res1 = a5
+elif a5 >= res1:
+    res1 = a5
 
-if a2 >= a1:
-    if a2 >= a3:
-        if a2 >= a4:
-            if a2 >= a5:
-                res = a2
-            else:
-                res = a5
-
-if a3 >= a1:
-    if a3 >= a2:
-        if a3 >= a4:
-            if a3 >= a5:
-                res = a3
-            else:
-                res = a5
-if a4 >= a1:
-    if a4 >= a2:
-        if a4 >= a3:
-            if a4 >= a5:
-                res = a4
-            else:
-                res = a5
-if a5 >= a1:
-    if a5 >= a2:
-        if a5 >= a3:
-            if a5 >= a4:
-                res = a5
-            else:
-                res = a4
-print(f'Максимум {res}')
+print(f'Максимум {res1}')
 print(f'Список чисел для удобства: {result}')
