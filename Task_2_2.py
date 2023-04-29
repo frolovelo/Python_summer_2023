97bd1564a5997a1120d2329ffa47d1cde7ddb8e3
Daniil Frolov <frolofelo@gmail.com>
2023-04-29 12:01:32 +0300

    Минимум списка

---
+++ Task_2_2.py
@@ -0,0 +1,8 @@
+from random import *
+lst = [randint(1,50) for i in range(10)]
+m = lst[0]
+for i in lst[1:]:
+    if i < m:
+        m = i
+print(f'Минимум: {m} из списка {lst}')
+print(f'Функция min: {min(lst)}')
