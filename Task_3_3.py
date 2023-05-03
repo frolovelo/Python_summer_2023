b6b1fcd1b284ec46b6c8edd10bdd6cf988449ad0
Daniil Frolov <frolofelo@gmail.com>
2023-05-03 23:09:28 +0300

    Самые длинные слова в предложении

---
+++ Task_3_3.py
@@ -0,0 +1,15 @@
+text = input().split()
+m = len(text[0])
+# находим максимум, можно было и так: len(max(text))
+for i in text:
+    if len(i) > m:
+        m = len(i)
+# находим все слова равные максимуму
+lst = []
+for i in text:
+    if len(i) == m:
+        lst.append(i)
+print(*lst)
+
+# функция однострочка
+# print(*list(filter(lambda x: len(x) == len(max(text)), text)))
