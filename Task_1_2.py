38b55fc8b813e7dcd66ed51ef52a17c8c9507214
Daniil Frolov <frolofelo@gmail.com>
2023-05-11 19:33:02 +0300

    Нахождение максимума

--- Task_1_2.py
+++ Task_1_2.py
@@ -5,38 +5,17 @@ a3 = x * y
 a4 = x / y
 a5 = x // y
 result = [a1, a2, a3, a4, a5]
-res1 = a1
+res = a1
 
-if a2 >= res1:
-    res1 = a2
-    if a3 >= res1:
-        res1 = a3
-        if a4 >= res1:
-            res1 = a4
-            if a5 >= res1:
-                res1 = a5
-        elif a5 >= res1:
-            res1 = a5
-    elif a4 >= res1:
-        res1 = a4
-        if a5 >= res1:
-            res1 = a5
-    elif a5 >= res1:
-        res1 = a5
-elif a3 >= res1:
-    res1 = a3
-    if a4 >= res1:
-        res1 = a4
-        if a5 >= res1:
-            res1 = a5
-    elif a5 >= res1:
-        res1 = a5
-elif a4 >= res1:
-    res1 = a4
-    if a5 >= res1:
-        res1 = a5
-elif a5 >= res1:
-    res1 = a5
+if res < a2:
+    res = a2
+if res < a3:
+    res = a3
+if res < a4:
+    res = a4
+if res < a5:
+    res = a5
 
-print(f'Максимум {res1}')
+
+print(f'Максимум {res}')
 print(f'Список чисел для удобства: {result}')
