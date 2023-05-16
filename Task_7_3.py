79c0de3455355ec710e81b19bf0cfc044df4acae
Daniil Frolov <frolofelo@gmail.com>
2023-05-16 13:13:32 +0300

    3 максимальных числа в матрице

---
+++ Task_7_3.py
@@ -0,0 +1,23 @@
+from math import inf
+
+
+def matrix(n, m, mat):
+    m1, m2, m3 = -inf, -inf, -inf
+    for i in range(n):
+        for j in range(m):
+            if mat[i][j] > m1:
+                m3 = m2
+                m2 = m1
+                m1 = mat[i][j]
+            elif mat[i][j] > m2:
+                m3 = m2
+                m2 = mat[i][j]
+            elif mat[i][j] > m3:
+                m3 = mat[i][j]
+
+    return [m3, m2, m1]
+
+
+n, m = 2, 3
+x = [[6, 2, 3], [1, 2, 4]]
+print(matrix(n, m, x))
