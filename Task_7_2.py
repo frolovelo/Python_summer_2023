4c62acbb97a7480b698e349e4b83b58197c8e771
Daniil Frolov <frolofelo@gmail.com>
2023-05-16 12:34:25 +0300

    Шифр цезаря

---
+++ Task_7_2.py
@@ -0,0 +1,16 @@
+def cezar(text, n):
+    lower_alp = 'abcdefghijklmnopqrstuvwxyz'
+    upper_alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    t = ''
+    n = n % 26
+    for i in text:
+        if i.isalpha():
+            if i.islower():
+                t += lower_alp[(lower_alp.find(i) + n) % 26]
+            if i.isupper():
+                t += upper_alp[(upper_alp.find(i) + n) % 26]
+        else:
+            t += i
+
+    return t
+print(cezar(input('Введите текст для зашифровки: '), int(input('Сдвиг: '))))
