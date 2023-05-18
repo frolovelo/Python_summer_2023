a842061709ef414e9861eb0c1026e7711dec5dc2
Daniil Frolov <frolofelo@gmail.com>
2023-05-18 16:27:10 +0300

    Сортировка по кол-ву уникальных букв и алфавиту

--- Task_8_3.py
+++ Task_8_3.py
@@ -1,11 +1,2 @@
-
-lst = [[1, 5, 3], [44, 2, 1], [1, 3]]
-def f(l):
-    t = ''
-    for i in l:
-        t += str(i)
-    return len(t)
-l = sorted(lst, key=lambda x: f(x))
-print(l)
-l1 = sorted(lst, key=lambda x: len(''.join([str(j) for j in x])))
-print(l1)
+t = [i.strip() for i in input('Введите слова через запятую: ').split(',')]
+print(sorted(t, key=lambda x: (-len(set(x)), x)))
