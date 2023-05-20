8e5663048d7997a2d1d95d3a67453ade6442915e
Daniil Frolov <frolofelo@gmail.com>
2023-05-20 12:05:18 +0300

    ДНК в РНК

---
+++ Task_9_1.py
@@ -0,0 +1,15 @@
+def dnk_to_rnk(dnk):
+    d = {'G': 'C',
+         'T': 'A',
+         'C': 'G',
+         'A': 'T'}
+    rnk = ''
+    for i in dnk:
+        n = d.get(i, 0)
+        if n == 0:
+            return 'Это не ДНК!'
+        rnk += n
+    return rnk
+
+
+print(dnk_to_rnk(input('Введите ДНК: ')))
