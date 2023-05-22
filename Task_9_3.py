e754019330db985302e76eebf4701023ba459350
Daniil Frolov <frolofelo@gmail.com>
2023-05-22 19:06:30 +0300

    Частотный анализ

--- Task_9_3.py
+++ Task_9_3.py
@@ -1,7 +1,6 @@
 with open('text.txt', 'r', encoding='utf-8') as f:
     d = {}
-    for i in f.read():
-        i = i.lower()
+    for i in f.read().lower():
         d[i] = d.get(i, 0) + 1
     d_sort = sorted(d, key=lambda x: (-d[x], x))
     c = 0
