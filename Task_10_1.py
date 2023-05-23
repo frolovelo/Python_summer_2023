aa2d7abe6990af948e5b7ed09c6d78b892fdc04c
Daniil Frolov <frolofelo@gmail.com>
2023-05-23 13:29:58 +0300

    openpyxl зп сотрудников

---
+++ Task_10_1.py
@@ -0,0 +1,18 @@
+import openpyxl
+wb = openpyxl.load_workbook('zp.xlsx')
+ws = wb.active
+d = {}
+for i in range(1, ws.max_row + 1):
+    name = ws.cell(i, 1).value
+    val = int(ws.cell(i, 2).value)
+    d[name] = d.get(name, 0) + val
+d['Итого'] = d.get('Итого', sum(d.values()))
+wb.create_sheet('Result')
+wb.active = wb['Result']
+ws = wb.active
+i = 1
+for k, v in d.items():
+    ws.cell(i, 1).value = k
+    ws.cell(i, 2).value = v
+    i += 1
+wb.save('zp.xlsx')
