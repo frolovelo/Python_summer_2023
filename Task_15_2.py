f3c0f823b108bc6b4bb3cbc618619d859320b18b
Daniil Frolov <frolofelo@gmail.com>
2023-06-03 00:50:36 +0300

    re авто-номера

--- Task_15_2.py
+++ Task_15_2.py
@@ -1,4 +1,4 @@
 import re
 text = input('Введите номера через пробел: ')
 pattern = r'[ETOPAHKXCBM]'
-print(re.findall(rf"\b{pattern}\d\d\d{pattern}{pattern}78\b|\b{pattern}\d\d\d{pattern}{pattern}178\b", text))
+print(*re.findall(rf"\b{pattern}\d\d\d{pattern}{pattern}78\b|\b{pattern}\d\d\d{pattern}{pattern}178\b", text))
