f19fb0fa28699c3bc98b5adf8fa71ed64efca2dd
Daniil Frolov <frolofelo@gmail.com>
2023-06-03 11:26:15 +0300

    re отбор корректных телефонных номеров

--- Task_15_3.py
+++ Task_15_3.py
@@ -6,12 +6,12 @@ def find_phone_numbers(text):
     code2 = r'921'
     pat1 = r'\d{3}-\d{4}'
     pat2 = r'\d{3}-\d{2}-\d{2}'
-    regex1 = rf'\B\+7\({code1}\){pat1}\b'
-    regex2 = rf'\B\+7\({code1}\){pat2}\b'
-    regex3 = rf'\B\+7\({code2}\){pat1}\b'
-    regex4 = rf'\B\+7\({code2}\){pat2}\b'
+    regex = rf'\B\+7\({code1}\){pat1}\b|'
+    regex += rf'\B\+7\({code1}\){pat2}\b|'
+    regex += rf'\B\+7\({code2}\){pat1}\b|'
+    regex += rf'\B\+7\({code2}\){pat2}\b'
 
-    numb = re.findall(regex1, text) + re.findall(regex2, text) + re.findall(regex3, text) + re.findall(regex4, text)
+    numb = re.findall(regex, text)
     return numb
 
 
