2733f33ef4c71f9d200e58b9031be04d968961a7
Daniil Frolov <frolofelo@gmail.com>
2023-06-05 19:08:03 +0300

    рекурсия словрь словарей

--- Task_15_1.py
+++ Task_15_1.py
@@ -1,15 +1,15 @@
 d = {4: 1, 3: {4: 3, 3: {1: 6, 3: 1}, 2: 3}, 2: {1: 4}, 1: {2: 6}, 5: 2}
-res = []
 
 def rec(d, key):
+    res = []
     for k, v in d.items():
-        if type(v) == dict:
-            if key == k:
-                res.append(v)
-            rec(v, key)
-        elif key == k:
+        if key == k:
             res.append(v)
+        if type(v) == dict:
+            res.extend((rec(v, key)))
+    return res
+
 
 k = int(input('Введите ключик: '))
-rec(d, k)
-print(res if len(res) != 0 else f'Значений по ключу "{k}" не найдено!')
+print(rec(d, k))
+
