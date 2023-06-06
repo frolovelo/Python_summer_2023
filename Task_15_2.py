35e5ab7a17bf6fdc87099f4bb95450f8f40cc9f3
Daniil Frolov <frolofelo@gmail.com>
2023-06-06 12:24:35 +0300

    рекурсия и re

--- Task_15_2.py
+++
@@ -1,4 +0,0 @@
-import re
-text = input('Введите номера через пробел: ')
-pattern = r'[ETOPAHKXCBMКЕНХВАРОСМT]'
-print(*re.findall(rf"\b{pattern}\d\d\d{pattern}{pattern}78\b|\b{pattern}\d\d\d{pattern}{pattern}178\b", text))
