21459f8943eba8901c00f4e8788ce981e468abfb
Daniil Frolov <frolofelo@gmail.com>
2023-06-08 15:54:55 +0300

    декоратор args kwargs

--- Task_17_2.py
+++ Task_17_2.py
@@ -1,15 +1,16 @@
 def deco(func):
     def wrapper(*args, **kwargs):
         lst = []
-        val = func(*args, **kwargs)
-        lst.extend([i.upper() for i in val if type(i) == str])
+        func(*args, **kwargs)
+        [lst.append(i) for i in args if isinstance(i, str)]
+        [lst.append(i) for i in kwargs.values() if isinstance(i, str)]
         return lst
     return wrapper
 
 
 @deco
-def test(t):
-    return t
+def test(*args, **kwargs):
+    return
 
 
-print(test([1, 'to be or not to be', ['3', 1], {'4': '5'}, 'a hero']))
+print(test(1, 'to be or not to be', ['3', 1], {'4': '5'}, 'a hero', my='Даниил', brain='просто не понял задания...', test=5))
