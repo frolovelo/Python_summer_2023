28a4c1dd0be7dc355b66711c37f20f0e187fef3e
Daniil Frolov <frolofelo@gmail.com>
2023-06-12 19:30:47 +0300

    Классы ученики и учитель

--- Task_18.py
+++ Task_18.py
@@ -4,9 +4,9 @@ from colorama import Fore, Style
 colorama.init()
 
 '''Результаты было решено учитывать статусом:
-    None - задано домашнее задание
-    Done - выполнено учеником
-    Зачёт\Незачёт - в случае успешной проверки учителем'''
+    1.None - задано домашнее задание
+    2.Done - выполнено учеником
+    3.Зачёт\Незачёт - в случае проверки учителем'''
 
 
 class DataHometask:
@@ -47,8 +47,11 @@ class Teacher:
         status = 'Зачёт' if status else 'Незачёт'
         for task in info:
             if task in self.homework[people.name]:
-                self.homework[people.name][task] = status
-                people.take_homework(task, status=status)
+                if self.homework[people.name][task] is not None:
+                    self.homework[people.name][task] = status
+                    people.take_homework(task, status=status)
+                else:
+                    print(Fore.RED + f'Учитель - будь внимательнее!\n{people.name} даже не начинал делать {task} - до "{status}" не дотягивает.' + Style.RESET_ALL)
             else:
                 print(Fore.RED + f'{task} - не задавалось, поэтому и {status} некуда ставить...' + Style.RESET_ALL)
 
@@ -65,14 +68,16 @@ class Pupil:
 
     def complit_any_hometask(self, info):
         '''Выполянет домашку по отдельной теме'''
-        if info in self.homework:
+        if info in self.homework.keys():
             self.homework[info] = 'Done'
+            self.send_homework(marIvanna)
         else:
             print(Fore.RED + f'Домашнего задания - {info} не задавали!' + Style.RESET_ALL)
 
     def complit_all_hometask(self):
         '''Выполянет всю домашку'''
         self.homework = {key: 'Done' for key in self.homework}
+        self.send_homework(marIvanna)
 
     def send_homework(self, teacher):
         '''Отправляет домашку на проверку'''
@@ -89,16 +94,21 @@ pety = Pupil('Пётр')
 
 # Задали домашку
 marIvanna.set_homework(hometask[0], vasy, pety)
+marIvanna.set_homework(hometask[1], vasy)
 marIvanna.set_homework(hometask[2:], pety)
 
 print('То что было задано:', marIvanna.homework)
-print('У Петра информация по домашке:', pety.homework)
-#
+
+# Ученики выполняют домашнюю работу: Вася только 'Аннотации', а Петя всю, что задали (отправка на проверку предусмотрена сразу по выполнению)
 vasy.complit_any_hometask('Аннотации')
 pety.complit_all_hometask()
 
-pety.send_homework(marIvanna)
-marIvanna.check(pety, True, 'Генераторы')
-print()
-print('То что было задано:', marIvanna.homework)
-print('У Петра информация по домашке:', pety.homework)
+# Марьивановна решила проверить Генераторы и Декораторы у Пети и они оказались правильными - УРА!
+marIvanna.check(pety, True, 'Генераторы', 'Декораторы')
+# А вот Вася подкачал: Анотации сделал плохо, а ООП и вовсе не начинал делать:
+marIvanna.check(vasy, False, 'ООП', 'Аннотации')
+
+print('\nИтоговый результат:')
+print('На руках у Учителя:', marIvanna.homework)
+print(f'На руках у {vasy.name}: {vasy.homework}')
+print(f'На руках у {pety.name}: {pety.homework}')
