2d5391e271b219df48bea9c3bbd51311e954b606
Daniil Frolov <frolofelo@gmail.com>
2023-06-15 14:34:54 +0300

    itertools перебор купюр

--- Task_19_1.py
+++ Task_19_1.py
@@ -1,8 +1,8 @@
+'''itertools перебор купюр'''
 from itertools import *
 k = [50, 100, 200, 500, 1000, 2000, 5000]
-for i in range(2, len(k)+1):
-    status = 'купюры' if 2 <= i <= 4 else 'купюр'
-    print(f'{i} {status}: ', end='')
+for i in range(1, len(k)+1):
+    print(f'{i} куп: ', end='')
     for j in combinations(k, i):
         print(sum(j), end=',')
     print()
