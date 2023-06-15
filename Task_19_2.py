3f6095185c6f99f51fe50150187ca546fc57a8e5
Daniil Frolov <frolofelo@gmail.com>
2023-06-15 11:58:11 +0300

    class Фибоначчи

---
+++ Task_19_2.py
@@ -0,0 +1,23 @@
+class Fib:
+    def __init__(self):
+        self.a = 0
+        self.b = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        nx = self.b
+        self.a, self.b = self.b, self.a + self.b
+        return nx
+
+
+fib = Fib()
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(next(fib))
