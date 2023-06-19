7d85daee4a7db6314a4507991a8d7a372fb1cef6
Daniil Frolov <frolofelo@gmail.com>
2023-06-20 00:41:03 +0300

    itertools Генератор A,1-Z,26

---
+++ Check_in/Task_20_3.py
@@ -0,0 +1,24 @@
+'''itertools Генератор A,1-Z,26'''
+from itertools import cycle, chain
+
+
+class IterFrolTools:
+    def __init__(self):
+        self.chars = [chr(i) for i in range(65, 91)]
+        self.numbers = [i for i in range(1, 27)]
+        self.gen = cycle(chain.from_iterable(zip(self.chars, self.numbers)))
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        for i in self.gen:
+            return i
+
+
+az = IterFrolTools()
+for _ in range(80):
+    res = next(az)
+    print(res, end=',')
+    if res == 26:
+        print()
