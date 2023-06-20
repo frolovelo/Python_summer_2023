50425394d07ab05a9cbb288bef33eb7af859ded9
Daniil Frolov <frolofelo@gmail.com>
2023-06-20 14:54:05 +0300

    SQL таблица book добавление атрибутов и кортежей

---
+++ Task_21_3.sql
@@ -0,0 +1,7 @@
+CREATE TABLE book(id INT PRIMARY KEY, author text, title text, id_publisher int);
+INSERT INTO book VALUES (1, 'Ремарк Э.М.', 'Три товарища', 10),
+(2, 'Оруэлл Д.', '1984', 20),
+(3, 'Булгаков М.А.', 'Мастер и Маргарита', 30),
+(4, 'Достоевский Ф.М.', 'Идиот', 40);
+SELECT author, title FROM book;
+
