99b51fab2b70a916da4ea8e203957ea20a997b18
Daniil Frolov <frolofelo@gmail.com>
2023-06-27 00:15:50 +0300

    переместил

--- Task_23_3.py
+++
@@ -1,7 +0,0 @@
-'''Наибольшее число, которое можно составить из массива чисел'''
-lst = [9, 90, 92]
-for i in range(len(lst)-1):
-    for j in range(len(lst)-i-1):
-        if str(lst[j]) + str(lst[j+1]) < str(lst[j+1]) + str(lst[j]):
-            lst[j], lst[j+1] = lst[j+1], lst[j]
-print(''.join(map(str, lst)))
