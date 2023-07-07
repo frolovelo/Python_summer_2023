1ebd56cffcc512d16b92211980f083878eb09f3e
Daniil Frolov <frolofelo@gmail.com>
2023-07-07 16:23:36 +0300

    Ханойская башня

--- Task_28_3.py
+++ Task_28_3.py
@@ -6,7 +6,7 @@ def bashya(n, s2=1, s3=2):
     if n == 1:
         return 1
     else:
-        c += bashya(n - 1, s2, 6 - s2 - s3)
+        c += bashya(n - 1, s2, 6 - s2 - s3) + 1
         c += bashya(n - 1, 6 - s2 - s3, s3)
     return c
 
